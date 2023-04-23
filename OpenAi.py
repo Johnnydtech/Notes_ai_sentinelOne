eeb400ea8aca6fd3bb152215fcfb3605d8a708ad
Yohanes D Tech <36245619+Johnnydtech@users.noreply.github.com>
2023-04-22 21:17:06 -0400

    Update OpenAi.py

--- OpenAi.py
+++ OpenAi.py
@@ -8,14 +8,14 @@ class ThreatAnalyzer:
         self.openai_key = openai_key
         self.notes = None
 
-    def note_response(self, file_path, originating_process, commandLine_arg):
+    def note_response(self, file_path, originating_process, commandLine_arg, file_name):
         openai.api_key = self.openai_key
        # Note: you need to be using OpenAI Python v0.27.0 for the code below to work
         response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
                 {"role": "system", "content": "analyzes alerts for malicious content using multiple varaibles from the user."},
-                {"role": "user", "content": f"analyze the alert using the filepath, orignating process and command line argument provided {file_path}, {originating_process}, and {commandLine_arg}. If the data is insufficient, just use the filepath to analyze the alert and share you need more info for detailed analysis. Always show the filepath on your response. Additionally, classify the alert into theses groups ,Legitimate, Investigation needed or  Malicious. Meniton analysis is being done by a custom AI tool, at the beginning of the anlysis."}    
+                {"role": "user", "content": f"analyze the alert using the file name, filepath, orignating process and command line argument provided {file_name}, {file_path}, {originating_process}, and {commandLine_arg}. If the data is insufficient, just use the filepath to analyze the alert and share you need more info for detailed analysis. Always show the filepath on your response. Additionally, classify the alert into theses groups ,Legitimate, Investigation needed or  Malicious. Meniton analysis is being done by a custom AI tool, at the beginning of the anlysis."}    
             ]
         )
         self.notes = response['choices'][0]['message']['content']
